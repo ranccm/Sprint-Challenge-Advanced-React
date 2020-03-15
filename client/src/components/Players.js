60d83fc8336b258a8abadfc796f156e04e58cefa
ranccm <ranimnapier@gmail.com>
2020-03-15 13:46:09 -0700

    players rendering

---
+++ client/src/components/Players.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import axios from 'axios';
+
+class Players extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            players: []
+        }
+    }
+
+    componentDidMount() {
+        console.log("component did mount");
+        axios
+            .get('http://localhost:5000/api/players')
+            .then(res => { console.log(res.data); 
+            this.setState({ players: res.data });
+            });
+        }
+
+
+    render() {
+        return (
+            <div className="playerList">
+                    {this.state.players.map((item) => 
+                    <p>{item.name}</p>)}
+            </div>
+        );
+    }
+}
+
+export default Players;
